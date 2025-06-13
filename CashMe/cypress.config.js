8e699984bd679dccfd0bb478881898958e5cc172
Prognum-MarcosRibeiro <marcos.ribeiro@prognum.com.br>
2025-06-13 18:38:39 -0300

    Atualizando para executar os testes individuais da CashMe.

--- CashMe/cypress.config.js
+++ CashMe/cypress.config.js
@@ -2,14 +2,16 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: 'CashMe/cypress/support/e2e.js',
+    specPattern: 'CashMe/cypress/e2e/**/*.cy.js',
+    screenshotsFolder: 'CashMe/cypress/screenshots',
+    videosFolder: 'CashMe/cypress/videos',
     reporter: 'mochawesome',
     reporterOptions: {
-      reportDir: 'cypress/reports/html',
-      overwrite: true,
+      reportDir: 'CashMe/cypress/reports/html',
+      overwrite: false,
       html: true,
-      json: false,
-      inlineAssets: true,
-      charts: true
+      json: true
     }
-  }
+  },
 })
